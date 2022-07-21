2a22a7090cb1c00a4861566d1b66c2afcba03591
Jakub Nowak <kuba9818@gmail.com>
2022-07-21 22:32:22 +0200

    Dodaj pliki projektów.

---
+++ KursyWalut/PomocnikNbp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace KursyWalut
+{
+    public static class PomocnikNbp
+    {
+        private static KursyWalutNBP parsujPozycjęTabeliKursówWalutNBP(XElement elementPozycja,
+                                                                       IFormatProvider formatProvider)
+        {
+            KursyWalutNBP pozycja = new KursyWalutNBP();
+            pozycja.NazwaWaluty = elementPozycja.Element("nazwa_waluty").Value;
+            pozycja.Przelicznik = double.Parse(elementPozycja.Element("przelicznik").Value);
+            pozycja.KodWaluty = elementPozycja.Element("kod_waluty").Value;
+            pozycja.KursKupna = decimal.Parse(elementPozycja.Element("kurs_kupna").Value);
+            pozycja.KursSprzedaży = decimal.Parse(elementPozycja.Element("kurs_sprzedazy").Value);
+
+            return pozycja;
+        }
+
+        private static string pobierzPlik(string adres)
+        {
+            using HttpClient klientSieciowy = new HttpClient();
+            using Stream strumień = klientSieciowy.GetStreamAsync(adres).Result;
+            return new StreamReader(strumień).ReadToEnd();
+        }
+
+        public static TabelaKursówWalutNBP PobierzAktualnąTabeleKursówNBP()
+        {
+            string adres = "https://www.nbp.pl/kursy/xml/LastC.xml";
+            string zawartośćPlikuXml = pobierzPlik(adres);
+            XDocument xml = XDocument.Parse(zawartośćPlikuXml);
+
+            IFormatProvider formatProvider = new CultureInfo("pl");
+
+            TabelaKursówWalutNBP tabela = new TabelaKursówWalutNBP();
+            tabela.NumerTabeli = xml.Root.Element("numer_tabeli").Value;
+            tabela.DataNotowania = DateTime.Parse(xml.Root.Element("data_notowania").Value,
+                                                                   formatProvider);
+            tabela.DataPublikacji = DateTime.Parse(xml.Root.Element("data_publikacji").Value,
+                                                                    formatProvider);
+            tabela.Pozycje = new Dictionary<string, KursyWalutNBP>();
+            foreach (XElement elementPozycja in xml.Root.Elements("pozycja"))
+            {
+                KursyWalutNBP pozycja = parsujPozycjęTabeliKursówWalutNBP(elementPozycja, formatProvider);
+                tabela.Pozycje.Add(pozycja.KodWaluty, pozycja);
+            }
+            return tabela;
+        }
+    }
+    public struct KursyWalutNBP
+    {
+        public string NazwaWaluty;
+        public string KodWaluty;
+        public double Przelicznik;
+        public decimal KursKupna;
+        public decimal KursSprzedaży;
+
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return KodWaluty + " " + KursKupna.ToString(formatProvider) + " - " + KursSprzedaży.ToString(formatProvider);
+        }
+
+        public override string ToString()
+        {
+            return ToString(new CultureInfo("pl"));
+        }
+    }
+    public struct TabelaKursówWalutNBP
+    {
+        public string NumerTabeli;
+        public DateTime DataNotowania;
+        public DateTime DataPublikacji;
+        public Dictionary<string, KursyWalutNBP> Pozycje;
+    }
+}
---
+++ KursyWalut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KursyWalut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TabelaKursówWalutNBP tabela =
+                PomocnikNbp.PobierzAktualnąTabeleKursówNBP();
+                string s = "Tabela kursów walut";
+                s += "\n\nNumer tabeli: " + tabela.NumerTabeli;
+                s += "\nData notowania: " + tabela.DataNotowania.ToLongDateString();
+                s += "\nData publikacji: " + tabela.DataPublikacji.ToLongDateString();
+                foreach (KeyValuePair<string, KursyWalutNBP> pozycja in tabela.Pozycje)
+                {
+                    s += "\n" + pozycja.Value.ToString();
+                }
+                Console.WriteLine(s);
+            }
+            catch (Exception exc)
+            {
+                Console.Error.WriteLine(
+                $"Błąd podczas pobierania kursów walut NBP: {exc.Message}");
+            }
+        }
+    }
+}
